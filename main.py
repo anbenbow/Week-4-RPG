4f0c060776ad59fc685cb79fef3767cb688b8665
Amber Benbow <anbenbow@gmail.comgit config --global user.email anbenbow@gmail.com>
2022-08-11 22:03:18 -0400

    Updated my_hero File to include hero dictionary

---
+++ main.py
@@ -0,0 +1,16 @@
+hero = {
+    'name': 'Sleeping Beauty',
+    'level': 7,
+    'health': 100,
+    'equipment': {'pajamas', 'sleep mask', 'travel pillow'},
+    'attacks': (('Weighted Blanket', 95),('Lavender Diffuser', 75),('White Noise', 50),('Chamomile Tea', 25)),
+    'coins': {
+        'copper': 700,
+        'silver': 700,
+        'gold': 700,
+    },
+    'phrases': {
+        'challenges': ['YOU WILL NOT BEAT MEEEEEEEE!!!!!!!!','BWAHAHAHAHAHAHAHAHAHA!!!!!!!!','THAT ALL YOU GOT?!'],
+        'pleas': ['AAAAAHHHHHHHHHHHH!!!!!!!!','OH NO! MY SLEEP MASK!','OUCH! OUCH!! OUCH!!!','STOP! STOP! IM TIRED!!!'],
+    }
+}
