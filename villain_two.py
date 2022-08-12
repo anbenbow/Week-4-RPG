4f178748584980ef40596106655910820c9c7d74
Amber Benbow <anbenbow@gmail.comgit config --global user.email anbenbow@gmail.com>
2022-08-11 22:15:53 -0400

    Updated villian_two to include Cinderella dictionary

---
+++ villain_two.py
@@ -0,0 +1,16 @@
+villian_two = {
+    'name': 'Cinderella',
+    'level': 9,
+    'health': 100,
+    'equipment': {'Swarovski Crystal Slippers'},
+    'attacks': (('Glass Slippers', 95),('Fuzzy Slippers', 75),('Flip Flops', 50),('Socks', 25)),
+    'coins': {
+        'copper': 100,
+        'silver': 100,
+        'gold': 100,
+    },
+    'phrases': {
+        'challenges': ['YOU ARE NO MATCH FOR ME, BEAUTY!!!!!!!!','BWAHAHAHAHAHAHAHAHAHA!!!!!!!!','THAT ALL YOU GOT?!'],
+        'pleas': ['AAAAAHHHHHHHHHHHH!!!!!!!!','OH NO! MY SLIPPERS!','OUCH! OUCH!! OUCH!!!','STOP! STOP! NO MORE!!!'],
+    }
+}
