439761bdc0bc0bb082773e3a1c71409bc04e61c7
Amber Benbow <anbenbow@gmail.comgit config --global user.email anbenbow@gmail.com>
2022-08-11 22:23:45 -0400

    Updated villain_three to include The Energizer Bunny dictionary

---
+++ villain_three.py
@@ -0,0 +1,16 @@
+villian_three = {
+    'name': 'The Energizer Bunny',
+    'level': 10,
+    'health': 100,
+    'equipment': {'Bunny Ears'},
+    'attacks': (('Insomnia', 100),('Alarm Clocks', 95),('Loud Music', 50),('Bright Lights', 25)),
+    'coins': {
+        'copper': 100,
+        'silver': 100,
+        'gold': 100,
+    },
+    'phrases': {
+        'challenges': ['WAKE UP, BEAUTY!!!!!!!!','BWAHAHAHAHAHAHAHAHAHA!!!!!!!!','YOU ARE NO MATCH FOR ME!!! HAHAHAHA!!!!!'],
+        'pleas': ['AAAAAHHHHHHHHHHHH!!!!!!!!','OH NO! MY BATTERIES!','OUCH! OUCH!! OUCH!!!','STOP! STOP! NO MORE!!!'],
+    }
+}
